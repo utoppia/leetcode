fcea78e5b97797dea73ae9e6cc7551a09d8baa2a
utoppia <utoppia@163.com>
2021-08-27 22:36:52 +0800

    add solution of leetcode #1081 Smallest Subsequence Of Distinct Characters

---
+++ 1081_smallest_subsequence_of_distinct_characters.py
@@ -0,0 +1,35 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        # same with leetcode 316
+        cnt = {}
+        for ch in s:
+            if ch in cnt:
+                cnt[ch] += 1
+            else:
+                cnt[ch] = 1
+                
+        st = []
+        # status marks whether a char is in the stack
+        # use bit XOR operation
+        status = 0 
+        for ch in s:
+            # if 'ch' was in the stack
+            if status ^ (1 << (ord(ch) - ord('a'))) < status:
+                cnt[ch] -= 1
+                continue
+
+            # if the top of stack is greater than current char
+            # and there still has char in the last string
+            # pop the top of stack and modify the status
+            while st and ord(st[-1]) > ord(ch) and cnt[st[-1]]:
+                k = st.pop()
+                status ^= (1 << (ord(k) - ord('a')))
+
+            cnt[ch] -= 1
+            
+            # add current char to the stack
+            st.append(ch)
+            # update the status
+            status ^= (1 << (ord(ch) - ord('a')))
+
+        return ''.join(st)
