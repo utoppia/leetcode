87a52d083c41394e8d1fb364a09b888191a6812e
utoppia <utoppia@163.com>
2021-09-19 01:27:16 +0800

    add solution of leetcode #42 Trapping Rain Water

---
+++ DynamicProgramming/42_trapping-rain-water.py
@@ -0,0 +1,19 @@
+class Solution:
+  def trap(self, height: List[int]) -> int:
+
+    st = [(0, height[0])]
+
+    ans = pre = 0
+    n = len(height)
+
+    for i in range(1, n):
+      while st and st[-1][1] <= height[i]:
+        j, h = st.pop(-1)
+        ans += (h - pre) * (i - j - 1)
+        pre = h
+      if st:
+        ans += (height[i] - pre) * (i - st[-1][0] - 1)
+      st.append((i, height[i]))
+      pre = height[i]
+
+    return ans
