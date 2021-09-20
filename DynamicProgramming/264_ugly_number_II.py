276169a4edc47c976c14d06c899bd57295e38581
utoppia <utoppia@163.com>
2021-09-21 00:08:27 +0800

    add solution of leetcode #264 Ugly Number II

---
+++ DynamicProgramming/264_ugly_number_II.py
@@ -0,0 +1,20 @@
+from heapq import heappush, heappop
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        # heap and set
+        queue = [1]
+        cur = None
+        s = set([1])
+        for _ in range(n):
+            cur = heappop(queue)
+            if 2 * cur not in s:
+                s.add(2 * cur)
+                heappush(queue, 2 * cur)
+            if 3 * cur not in s:
+                s.add(3 * cur)
+                heappush(queue, 3 * cur)
+            if 5 * cur not in s:
+                s.add(5 * cur)
+                heappush(queue, 5 * cur)
+        return cur
+
