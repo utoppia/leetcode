c4e8009546b8cdc7a6a55b117e89b1e400aa10d7
utoppia <utoppia@163.com>
2021-09-26 00:36:27 +0800

    add solution of leetcode #64 Minimum Path Sum

--- DynamicProgramming/64_minimum-path-sum.py
+++ DynamicProgramming/64_minimum-path-sum.py
@@ -2,9 +2,10 @@ class Solution:
   def minPathSum(self, grid: List[List[int]]) -> int:
     n, m = len(grid), len(grid[0])
     dp = [grid[0][i] for i in range(m)]
+    for j in range(1, m):
+      dp[j] += dp[j-1]
 
     for i in range(1, n):
-      print(dp)
       for j in range(m):
         dp[j] += grid[i][j]
         if j >= 1:
