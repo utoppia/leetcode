57d60395148e3344934b607be0a30e336870ed51
utoppia <utoppia@163.com>
2021-09-29 22:23:52 +0800

    add solution of leetcode #392 Is Subsequence

---
+++ DynamicProgramming/392_is-subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+  def isSubsequence(self, s: str, t: str) -> bool:
+    i, j = 0, 0
+    while i < len(s) and j < len(t):
+      while j < len(t) and s[i] != t[j]:
+        j += 1
+
+      if j < len(t) and s[i] == t[j]:
+        i += 1
+        j += 1
+
+    return i == len(s)
